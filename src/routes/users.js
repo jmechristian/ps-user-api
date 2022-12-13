7080914e77d07bc2beacd530a799e38267e036bd
jmechristian <jmechristian@gmail.com>
2022-12-12 20:14:48 -0500

    funny

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,10 @@ const UserRepo = require('../repos/user-repo');
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+  res.send('Welcome to the Packaging Show.');
+});
+
 router.get('/users', async (req, res) => {
   const users = await UserRepo.find();
   res.send(users);
